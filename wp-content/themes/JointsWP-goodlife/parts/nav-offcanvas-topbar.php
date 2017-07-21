53007323ef2ae543ef276d95a366035759d8204a
bradhartshorn <hartshorn.bradley@gmail.com>
2017-07-21 13:43:49 -0600

    SVG logo added to center of Nav, trying to verticle-center align nav elements

--- wp-content/themes/JointsWP-goodlife/parts/nav-offcanvas-topbar.php
+++ wp-content/themes/JointsWP-goodlife/parts/nav-offcanvas-topbar.php
@@ -1,33 +1,29 @@
 <!-- By default, this menu will use off-canvas for small
 	 and a topbar for medium-up. Components below listed left to right, Desktop to Mobile. -->
 
-<div class="top-bar" id="top-bar-menu">
+<div class="row">
+	<div class="top-bar" id="top-bar-menu">
 
 		<!-- Site Identity -->
-	<div class="text-center">
-<a href="<?php echo home_url(); ?>">GLB Logo Here</a>
-	</div>
+		<div class="text-center">
+			<a href="<?php echo home_url(); ?>"><div class="sitelogo"><?php get_template_part( 'assets/images/sitelogo'); ?></div></a>
+		</div>
 
-	<!-- Social Media Widget -->
-	<div class="top-bar-left float-left">
-		<?php get_sidebar('social'); ?>
-	</div>
+		<!-- Social Media Widget -->
+		<div class="top-bar-left float-left">
+			<?php get_sidebar('social'); ?>
+		</div>
 
+		<!-- Desktop Nav -->
+		<div class="top-bar-right show-for-medium">
+			<?php joints_top_nav(); ?>	
+		</div>
 
+	    <!-- Mobile Menu Toggle -->
+		<div class="top-bar-right show-for-small-only btn-mobile-nav">
+			<button class="list-container" data-toggle="off-canvas"><span class="fi-list"></span></button>
+			<a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a>
+		</div>
 
-	<!-- Desktop Nav -->
-	<div class="top-bar-right show-for-medium">
-		<?php joints_top_nav(); ?>	
-	</div>
-
-    <!-- Mobile Menu Toggle -->
-	<div class="top-bar-right float-right show-for-small-only">
-		<ul class="menu">
-			<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
-			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
-		</ul>
-	</div>
-
-</div><!-- end #top-bar-menu -->
-
-
+	</div><!-- end #top-bar-menu -->
+</div>
